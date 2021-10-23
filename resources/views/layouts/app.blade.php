4bbb554d5285781fccdb1141e429669fabde3797
rismafadila <rismaf651@gmail.com>
2021-10-23 14:50:04 +0700

    layouting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,13 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="fixed-bottom">
+<center>
+    Risma Septiani Fadila
+    - {{ date('Y') }}
+</center>
+        </footer>
     </div>
 </body>
 </html>
